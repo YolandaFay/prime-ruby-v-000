d134c0e47d4c830eb90c46bd8d9d90067a326a75
Yolanda Fay <yolandafay@gmail.com>
2018-09-22 20:55:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,18 @@ require 'math'
 
 def prime?(integer) 
   upper_limit = Math.sqrt(integer)
+  i = 0 
+  
+  if integer <= 0 
+    return false 
+  else 
+    while i < upper_limit 
+      if integer%i == 0 
+        return false 
+      else 
+        i += 1 
+      end
+    end
+    return true 
+  
 end
